b99fb92c175bc25b01432b534492b9db332cbdd1
Aditya Khandagale <154240086+Akhandagale06@users.noreply.github.com>
2026-04-22 23:03:17 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { ClerkProvider } from "@clerk/clerk-react";
 
 
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-console.log("CLERK KEY:", clerkPubKey);
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
